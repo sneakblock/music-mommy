330e4b8131a8f24f829ea738d5d84e806d54ce0e
mlee692 <68256202+michelle692@users.noreply.github.com>
2022-03-23 20:21:20 -0400

    circle.js

---
+++ js/circle.js
@@ -0,0 +1,33 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+
+
+new_circle(scene, 0.5, 30, 0xffff00);
+
+
+function animate() {
+    //requestAnimationFrame( animate );
+
+
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+function new_circle(scene, radius, segments, color) {
+    const geometry = new THREE.CircleGeometry(radius, segments);
+    const material = new THREE.MeshBasicMaterial( { color: color } );
+    const circle = new THREE.Mesh( geometry, material );
+    scene.add(circle);
+}
+
+
+animate();
