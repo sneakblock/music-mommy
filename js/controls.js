6980bfb7a4299f27103170331e090e7a466758f3
block3 <block3@gatech.edu>
2022-03-23 21:37:22 -0400

    WOW!

--- js/controls.js
+++ js/controls.js
@@ -1,13 +1,24 @@
 let canvas;
 let mic;
+let fft;
+let spectrum;
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight / 2);
+    noFill();
     mic = new p5.AudioIn();
     mic.start();
+    fft = new p5.FFT();
+    fft.setInput(mic);
 }
 
 function draw() {
-    background(51);
-    
+    background(240);
+    spectrum = fft.analyze();
+
+    beginShape();
+    for (i = 0; i < spectrum.length; i++) {
+        vertex(i, map(spectrum[i], 0, 255, height, 0));
+    }
+    endShape();
 }
