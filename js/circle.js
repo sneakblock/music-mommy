844e1af86cdcfac08f463e75982c2ccc87b7283c
block3 <block3@gatech.edu>
2022-03-23 20:53:33 -0400

    WOW

--- js/circle.js
+++ js/circle.js
@@ -1,14 +1,3 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight / 2), 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight / 2 );
-document.body.appendChild( renderer.domElement );
-
-camera.position.z = 5;
-
-
-
 new_circle(scene, 0.5, 30, 0xffff00);
 
 
