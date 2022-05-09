be0dee3f47e452ba35d684437806695ee799ec7e
mlee692 <68256202+michelle692@users.noreply.github.com>
2022-05-09 12:26:50 -0400

    colors

--- js/plane.js
+++ js/plane.js
@@ -3,6 +3,9 @@ let uniforms = {
     n_val: {type: 'float', value: 1.0}
 }
 
+// const textureLoader = new THREE.TextureLoader();
+// const text = textureLoader.load("/textures/texture1.jpg");
+
 const planeGeometry = new THREE.PlaneGeometry(64, 64, 64, 64);
     const planeCustomMaterial = new THREE.ShaderMaterial({
         uniforms: uniforms,
@@ -16,7 +19,19 @@ const planeGeometry = new THREE.PlaneGeometry(64, 64, 64, 64);
     planeMesh.scale.y = 2;
     planeMesh.scale.z = 2;
     planeMesh.position.y = 8;
+
+
+    // const sphereGeometry = new THREE.SphereGeometry(64, 64, 64);
+    // const sphereCustomMaterial = new THREE.ShaderMaterial({
+    //     uniforms: uniforms,
+    //     vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    //     fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+    //     wireframe: true,
+    // });
+    // const sphereMesh = new THREE.Mesh(sphereGeometry, sphereCustomMaterial);
+
     scene.add(planeMesh);
+    // scene.add(sphereMesh);
 
 function animate() {
     renderer.render( scene, camera );
